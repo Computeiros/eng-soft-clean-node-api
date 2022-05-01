8f93554a0260ac71afda8dc2a2bafc3c2a80472f
Lucas Flaquer <lucas.flaquer@timeprimo.com>
2022-05-01 12:23:56 -0300

    swagger docs url

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,10 @@
 import './config/module-alias';
+import swaggerUi from 'swagger-ui-express';
 import { env } from '@/main/config/env';
 import { app } from '@/main/config/app';
+import swaggerFile from '@/swagger.json';
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.listen(env.port, () => {
   console.log(`Server running at http://localhost:${env.port}`);
 });
